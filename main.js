44bf0db6896ac122bb9aa1f2174fa140e5202721
KhoaHenry <trananhkhoa73@gmail.com>
2022-01-04 14:40:15 +0700

    basic index

---
+++ main.js
@@ -0,0 +1,64 @@
+// $(".dropdown").click(function (e) {
+//     e.stopPropagation()
+//     let parent = e.target
+//     if (e.target.children.length === 0) {
+//         parent = e.target.parentNode
+//     }
+//     if (!parent.classList.contains("active"))
+//         parent.classList.add("active")
+//     else
+//         parent.classList.remove("active")
+// })
+
+// $(window).click(function () {
+//     if ($(".dropdown").hasClass("active"))
+//         $(".dropdown").removeClass("active")
+// })
+handleDropdown("dropdown")
+handleDropdown("action")
+
+function handleDropdown(className) {
+    $("." + className).click(function (e) {
+        e.stopPropagation()
+        let parent = e.target
+        if (e.target.children.length === 0) {
+            parent = e.target.parentNode
+        }
+        if (!parent.classList.contains("active"))
+            parent.classList.add("active")
+        else
+            parent.classList.remove("active")
+    })
+
+    $(window).click(function () {
+        if ($("." + className).hasClass("active"))
+            $("." + className).removeClass("active")
+    })
+}
+
+// display comment
+$(".comment-btn").click(function (e) {
+    const post = e.target.parentNode.parentNode
+    post.children[3].style.display = "block"
+})
+
+// display modal add post
+$("#add-post-input").click(function () {
+    $("#add-modal").modal("show")
+})
+
+// display list like
+$("#like-list").click(function () {
+    $("#like-list-modal").modal("show")
+})
+
+// display search list
+{
+    $("#search-input").focus(function () {
+        $(".search-list").css("display", "flex")
+    })
+
+    $("#search-input").focusout(function () {
+        $(".search-list").css("display", "none")
+    })
+}
